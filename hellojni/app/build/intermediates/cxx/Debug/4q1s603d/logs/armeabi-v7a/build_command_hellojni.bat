3ad432072f23f4415dd952b68141e01293e1fe32
shinhyeok.kang <kang930924@gmail.com>
2025-10-10 03:25:25 +0900

    jni

---
+++ hellojni/app/build/intermediates/cxx/Debug/4q1s603d/logs/armeabi-v7a/build_command_hellojni.bat
@@ -0,0 +1,12 @@
+@echo off
+"C:\\Users\\sh-new\\AppData\\Local\\Android\\Sdk\\ndk\\21.4.7075529\\ndk-build.cmd" ^
+  "NDK_PROJECT_PATH=null" ^
+  "APP_BUILD_SCRIPT=E:\\test\\android2\\ATAK-CIV-5.5.0.7-SDK\\ATAK-CIV-5.5.0.7-SDK\\samples\\hellojni\\app\\src\\main\\cpp\\Android.mk" ^
+  "NDK_APPLICATION_MK=E:\\test\\android2\\ATAK-CIV-5.5.0.7-SDK\\ATAK-CIV-5.5.0.7-SDK\\samples\\hellojni\\app\\src\\main\\cpp\\Application.mk" ^
+  "APP_ABI=armeabi-v7a" ^
+  "NDK_ALL_ABIS=armeabi-v7a" ^
+  "NDK_DEBUG=1" ^
+  "APP_PLATFORM=android-21" ^
+  "NDK_OUT=E:\\test\\android2\\ATAK-CIV-5.5.0.7-SDK\\ATAK-CIV-5.5.0.7-SDK\\samples\\hellojni\\app\\build\\intermediates\\cxx\\Debug\\4q1s603d/obj" ^
+  "NDK_LIBS_OUT=E:\\test\\android2\\ATAK-CIV-5.5.0.7-SDK\\ATAK-CIV-5.5.0.7-SDK\\samples\\hellojni\\app\\build\\intermediates\\cxx\\Debug\\4q1s603d/lib" ^
+  hellojni
