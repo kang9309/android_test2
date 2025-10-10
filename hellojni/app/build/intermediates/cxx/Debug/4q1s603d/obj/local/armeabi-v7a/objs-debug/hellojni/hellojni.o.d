85bbdc4bc651df506a54fb08ab7783b17305e2a8
shinhyeok.kang <kang930924@gmail.com>
2025-10-10 16:01:22 +0900

    build 파일 ignore

--- hellojni/app/build/intermediates/cxx/Debug/4q1s603d/obj/local/armeabi-v7a/objs-debug/hellojni/hellojni.o.d
+++
@@ -1,5 +0,0 @@
-E:\test\android2\ATAK-CIV-5.5.0.7-SDK\ATAK-CIV-5.5.0.7-SDK\samples\hellojni\app\build\intermediates\cxx\Debug\4q1s603d/obj/local/armeabi-v7a/objs-debug/hellojni/hellojni.o: \
-  E:\test\android2\ATAK-CIV-5.5.0.7-SDK\ATAK-CIV-5.5.0.7-SDK\samples\hellojni\app\src\main\cpp\hellojni.cpp \
-  E:\test\android2\ATAK-CIV-5.5.0.7-SDK\ATAK-CIV-5.5.0.7-SDK\samples\hellojni\app\src\main\cpp\..\..\..\build\generated\jni\com_atakmap_android_hellojni_plugin_HelloJNITool.h
-
-E:\test\android2\ATAK-CIV-5.5.0.7-SDK\ATAK-CIV-5.5.0.7-SDK\samples\hellojni\app\src\main\cpp\..\..\..\build\generated\jni\com_atakmap_android_hellojni_plugin_HelloJNITool.h:
